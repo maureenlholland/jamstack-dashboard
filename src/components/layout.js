32b08c4f76b762cff2c0ce8952c657d921f6bf9b
maureenlholland <maureen.holland11@gmail.com>
2020-06-07 11:27:49 -0400

    Add netlify identity

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { IdentityContextProvider } from 'react-netlify-identity-widget';
 
 import './layout.css';
 
 const Layout = ({ children }) => (
-    <>
+    <IdentityContextProvider url="https://jamstack-dashboard-auth.netlify.com">
         <header>
             <Link to ="/">Main Page</Link>
         </header>
         <main>
             {children}
         </main>
-    </>
+    </IdentityContextProvider>
 );
 
 export default Layout;
